b3a0b690deb731ec47955534fd9e22be666d0c03
Aneurin Barker Snook <a@aneur.in>
2021-10-08 18:41:23 +0100

    feat: add tx, wallet interactions, crypt functions

---
+++ lib/helpers.ts
@@ -0,0 +1,12 @@
+// Copyright (C) 2021 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+export const toQueryString = (data: Record<string, unknown>): string => Object.keys(data)
+  .map(key => `${key}=${urlsafe(data[key])}`)
+  .join('&')
+
+
+export const urlsafe = (v: unknown): string => {
+  if (typeof v === 'string') return v.replace(/ /g, '%20')
+  return `${v}`
+}
