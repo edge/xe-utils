81707aec35ad281f9f52d5c2a4da177cdf4ab2d0
Aneurin Barker Snook <a@aneur.in>
2021-10-25 16:27:49 +0100

    feat: add staking awareness

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a GNU GPL-style license
 // that can be found in the LICENSE.md file. All rights reserved.
 
+export * as stake from './stake'
 export * as tx from './tx'
 export * as wallet from './wallet'
 
---
+++ lib/stake.ts
@@ -0,0 +1,29 @@
+// Copyright (C) 2021 Edge Network Technologies Limited
+// Use of this source code is governed by a GNU GPL-style license
+// that can be found in the LICENSE.md file. All rights reserved.
+
+import superagent from 'superagent'
+
+export type Stake = {
+  amount: number
+  created: number
+  hash: string
+  id: string
+  released?: number
+  releaseTransaction?: string
+  transaction: string
+  type: StakeType
+  unlockPeriod: number
+  unlockRequested?: number
+  unlockTransaction?: string
+}
+
+export type Stakes = Record<string, Stake>
+
+export type StakeType = 'gateway' | 'host' | 'stargate'
+
+export const stakes = async (host: string, address: string): Promise<Stakes> => {
+  const url = `${host}/stakes/${address}`
+  const response = await superagent.get(url)
+  return response.body as Stakes
+}
